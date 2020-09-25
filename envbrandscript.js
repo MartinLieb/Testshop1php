aa84de7f28bdc165b214bec99d2dca46a674228f
Cosby1992 <Ande016j@edu.easj.dk>
2020-09-25 09:31:34 +0200

    added auth to request

--- envbrandscript.js
+++ envbrandscript.js
@@ -20,7 +20,7 @@ fetch("ghs.php?barcode=123&url=" + window.location.href)
         fetch(newurl.href, {
             method: 'POST',
             headers: new Headers({
-                "Authorization": `Basic ${base64.encode(`green:KormflaX3r`)}`
+                "Authorization": `Basic ${btoa('green:KormflaX3r')}`
             }),
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, // this line is important, if this content-type is not set it wont work
             body: newurl.search,
