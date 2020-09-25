d843204824426eb462f6d41a45ab664d48434e78
Cosby1992 <Ande016j@edu.easj.dk>
2020-09-25 09:44:20 +0200

    new request build

--- envbrandscript.js
+++ envbrandscript.js
@@ -17,24 +17,42 @@ fetch("ghs.php?barcode=123&url=" + window.location.href)
         let newurl = new URL(url);
         console.log("HREF" + newurl.href);
 
-        fetch(newurl.href, {
-            method: 'POST',
-            headers: new Headers({
-                "Authorization": 'Basic' + btoa('green:KormflaX3r')
-            }),
-            headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, // this line is important, if this content-type is not set it wont work
-            body: newurl.search,
-            mode: "no-cors",
+        var xhr = new XMLHttpRequest();
+        http.open("post", url, false, "green", "KormflaX3r");
+        xhr.open("post", url, false, "green", "KormflaX3r");
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                // show xhr.responseText somehow
+                document.getElementById('envbrand').innerHTML = xhr.responseText;
+                console.log("XHR STATUS TEXT (success) " + xhr.statusText);
+            }
+            else {
+                // handle failure, status code in xhr.status)
+                document.getElementById('envbrand').innerHTML = "FEJL";
+                console.log("XHR STATUS TEXT (failure) " + xhr.statusText);
+            }
+        }
+        xhr.send(newurl.search);
 
-        })
-            .then(res => res.text())
-            .then(data => {
-                console.log("DATA: " + data);
-            })
-            .catch(err => {
-                console.log("2: no brand for you pal");
-                console.log(err);
-            })
+        // fetch(newurl.href, {
+        //     method: 'POST',
+        //     headers: new Headers({
+        //         "Authorization": 'Basic' + btoa('green:KormflaX3r')
+        //     }),
+        //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' }, // this line is important, if this content-type is not set it wont work
+        //     body: newurl.search,
+        //     mode: "no-cors",
+
+        // })
+        //     .then(res => res.text())
+        //     .then(data => {
+        //         console.log("DATA: " + data);
+        //     })
+        //     .catch(err => {
+        //         console.log("2: no brand for you pal");
+        //         console.log(err);
+        //     })
 
     }).catch(err => {
         console.log("no brand for you pal");
